74fdc428c7135bf7a94011b4a2abe1febcad4af9
Diego Armando <00188625@uca.edu.sv>
2025-07-01 12:05:44 -0600

    Create mapa1.cpp

---
+++ mapa1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+
+//declaracion de la matriz de tipo char para la elaboracion del laberinto
+
+//laberinto nivel sencillo
+
+char laberinto [7][10] = {
+{'E',' ',' ','#','#','#','#','#','#',' '},
+{'#',' ','#','#','#',' ',' ','#',' ','#'},
+{' ',' ','#','#',' ',' ',' ',' ','#','#'},
+{' ',' ','#',' ',' ','#','#','#','S','#'},
+{' ','#','#',' ','#','#',' ',' ',' ','#'},
+{' ',' ',' ',' ','#',' ',' ','#','#',' '},
+{' ','#',' ',' ',' ',' ',' ','#','#','#'},
+};
+
+void mostrarlaberinto () {
+for (int i = 0; i < 7; i++){
+
+for (int j = 0; j < 10; j++){
+ cout << laberinto [i][j] << ' ';
+}
+cout << endl;
+}
+
+
+}
+//llamar a la funcion mostrarlaberinto en el main
