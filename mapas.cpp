440aa8e682e97b4d0f30a28613a86c5efc15ac35
Diego Armando <00188625@uca.edu.sv>
2025-07-01 18:37:38 -0600

    Update and rename mapa1.cpp to mapas.cpp

---
+++ mapas.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+//declaracion de la matriz de tipo char para la elaboracion del laberinto
+
+//laberinto nivel sencillo
+
+char laberinto [7][10] = {
+{'E',' ',' ','#','#','#','#','#','#',' '},
+{'#',' ','#','#','#',' ',' ','#',' ','#'},
+{' ',' ','#','#',' ',' ',' ',' ','#','#'},
+{' ',' ','#',' ',' ','#','#','#','S','#'},
+{' ','#','#',' ','#','#',' ',' ',' ','#'},
+{' ',' ',' ',' ','#',' ',' ','#','#',' '},
+{' ','#',' ',' ',' ',' ',' ','#','#','#'},
+};
+
+void mostrarlaberinto () {
+for (int i = 0; i < 7; i++){
+
+for (int j = 0; j < 10; j++){
+ cout << laberinto [i][j] << ' ';
+}
+cout << endl;
+}
+
+
+}
+//llamar a la funcion mostrarlaberinto en el main
+
+
+char laberinto [10][10] = {
+{'E','█','█','█','█','█','█','█',' ',' '},
+{' ',' ','█','█',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ','█','█','█','█'},
+{'█','█','█',' ',' ',' ',' ','█','█','█'},
+{'█','█',' ',' ',' ','█','█',' ',' ',' '},
+{' ',' ',' ',' ','█',' ','█',' ',' ','█'},
+{'█',' ','█',' ','█',' ',' ','█',' ',' '},
+{' ',' ',' ',' ','█','█',' ',' ',' ','█'},
+{' ','█',' ',' ',' ',' ','█','█',' ','S'},
+{'█','█',' ',' ','█',' ',' ',' ',' ',' '}
+};
+
+void mostrarlaberinto () {
+for (int i = 0; i < 10; i++){
+
+for (int j = 0; j < 10; j++){
+ cout << laberinto [i][j] << ' ';
+}
+cout << endl;
+}
+
+
+}
+//llamar a la funcion mostrarlaberinto en el main
